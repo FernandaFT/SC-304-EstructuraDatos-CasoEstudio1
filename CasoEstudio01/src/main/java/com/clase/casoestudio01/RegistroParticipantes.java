26176142f109ee213d918d46b948efb44130977d
FernandaFT <nanda199784@gmail.com>
2025-02-24 21:09:48 -0600

    adding the java classes

---
+++ CasoEstudio01/src/main/java/com/clase/casoestudio01/RegistroParticipantes.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.clase.casoestudio01;
+
+/**
+ *
+ * @author fernandafajardo
+ */
+public class RegistroParticipantes {
+    
+    private String nombreCompleto;
+    private int edad;
+    private String posicion;
+    private int numeroCamisa;
+
+    public RegistroParticipantes() {
+    }
+
+    public RegistroParticipantes(String nombreCompleto, int edad, String posicion, int numeroCamisa) {
+        this.nombreCompleto = nombreCompleto;
+        this.edad = edad;
+        this.posicion = posicion;
+        this.numeroCamisa = numeroCamisa;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getNumeroCamisa() {
+        return numeroCamisa;
+    }
+
+    public void setNumeroCamisa(int numeroCamisa) {
+        this.numeroCamisa = numeroCamisa;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro Participantes:" + 
+                "\nNombre Completo: " + nombreCompleto +
+                "\nEdad: " + edad + 
+                "\nPosición: " + posicion;
+    }  
+}
